fd0018bf78f544d02e626e7f468c6bd59e39c08e
Ronald Dilley <ronald.dilley.ctr@usgs.gov>
2016-10-06 14:34:56 -0500

    Adding

---
+++ gen-bridge-jobs.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+
+import os
+import sys
+import glob
+import json
+
+
+def main():
+
+    input_files = glob.glob('/data2/dilley/input-data/bridge/*')
+
+    with open('bridge-template.json', 'r') as template_fd:
+        template = template_fd.read()
+
+    jobs = list()
+
+    for input_file in input_files:
+        template_json = json.loads(template)
+        basename = os.path.basename(input_file)
+        input_product_id = basename.replace('.tar.gz', '')
+        input_url = 'file:///home/espa/input-data/bridge/{}'.format(basename)
+
+        template_json['order']['input-product-id'] = input_product_id
+        template_json['order']['input-url'] = input_url
+        jobs.append(template_json)
+
+    jobs_dict = {'jobs': jobs}
+    jobs_pretty = json.dumps(jobs_dict, indent=4, sort_keys=True)
+
+    with open('bridge-jobs.json', 'w') as jobs_fd:
+           jobs_fd.write(jobs_pretty)
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
