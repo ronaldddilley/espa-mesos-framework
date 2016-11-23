2f55e982aed20eb9bab9b2cbabe2a57f2f4c47b6
Ronald Dilley <ronald.dilley.ctr@usgs.gov>
2016-11-23 07:31:13 -0600

    Renamed and updated

--- gen-l2pgs-jobs.py
+++ gen-l2pgs-jobs.py
@@ -8,9 +8,9 @@ import json
 
 def main():
 
-    input_files = glob.glob('/data2/dilley/input-data/bridge/*')
+    input_files = glob.glob('/data2/dilley/input-data/bridge-ops-gen-scenes/*')
 
-    with open('bridge-template.json', 'r') as template_fd:
+    with open('l2pgs-template.json', 'r') as template_fd:
         template = template_fd.read()
 
     jobs = list()
@@ -19,7 +19,7 @@ def main():
         template_json = json.loads(template)
         basename = os.path.basename(input_file)
         input_product_id = basename.replace('.tar.gz', '')
-        input_url = 'file:///home/espa/input-data/bridge/{}'.format(basename)
+        input_url = 'file:///home/espa/input-data/bridge-ops-gen-scenes/{}'.format(basename)
 
         template_json['order']['input-product-id'] = input_product_id
         template_json['order']['input-url'] = input_url
@@ -28,7 +28,7 @@ def main():
     jobs_dict = {'jobs': jobs}
     jobs_pretty = json.dumps(jobs_dict, indent=4, sort_keys=True)
 
-    with open('bridge-jobs.json', 'w') as jobs_fd:
+    with open('l2pgs-jobs.json', 'w') as jobs_fd:
            jobs_fd.write(jobs_pretty)
 
     return 0
